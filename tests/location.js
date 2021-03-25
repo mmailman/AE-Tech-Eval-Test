123044882c9fa13292200e77ed19ef9278163267
Michael Mailman <mmailman@gmail.com>
2021-03-24 17:36:03 -0700

    Adding the last requirement for the Chicago verification text.

--- tests/location.js
+++ tests/location.js
@@ -1,16 +1,16 @@
 describe('Location', function() {
 
   //Test to Verify that the Chicago location is present in the footer and that info is correctly displayed
-  //title is "Chicago"
-  //Phone number "+1 312 729 4500"
   test('chicagoVerification', function(browser) {
     browser
       .url('https://www.designory.com')
       .click('.cookie-notice__close')
       .getLocationInView('footer')
       .pause(5000)
+      //Decided to use Xpath here to show that I can and the css selectors were getting kind of complex
       .useXpath()
       .assert.containsText('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', 'CHICAGO')
+      .assert.containsText('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/span', '+1 312 729 4500')
       //Looks like assert was returning an rgba code.  The rgba code for #808084 is rgba(128, 128, 132, 1)
       .assert.cssProperty('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', 'color', 'rgba(128, 128, 132, 1)', 'Testing if element <//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a> has an inactive font color of <#808084>')
       .moveToElement('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', 5, 5, function(){
