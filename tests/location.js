51ad4f88a9a9c6129cd71cef30d3c3541d8ce017
Michael Mailman <mmailman@gmail.com>
2021-03-24 17:00:14 -0700

    Initial Location test

---
+++ tests/location.js
@@ -0,0 +1,24 @@
+describe('Location', function() {
+
+  //Test to Verify that the Chicago location is present in the footer and that info is correctly displayed
+  //title is "Chicago"
+  //Phone number "+1 312 729 4500"
+  //Title inactive font color is "#808084"
+  //Title Hover color is "#fff"
+  test('chicagoVerification', function(browser) {
+    browser
+      .url('https://www.designory.com')
+      .click('.cookie-notice__close')
+      .getLocationInView('footer')
+      .pause(5000)
+      .useXpath()
+      .assert.containsText('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', 'CHICAGO')
+      //Looks like assert was returning an rgba code.  The rgba code for #808084 is rgba(128, 128, 132, 1)
+      .assert.cssProperty('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', 'color', 'rgba(128, 128, 132, 1)', 'Testing if element <//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a> has an inactive font color of <#808084>')
+      .assert.cssProperty('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', 'color', '#fff')
+      .end();
+      //.expect.element('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a').text.to.contain('CHICAGO')
+
+    //browser.expect.element('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a').to.have.css('color').which.contains('#808084')
+  });
+});
