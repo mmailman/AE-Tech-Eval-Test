91013c66d1761bbbafafdb2f85e35a59ad294958
Michael Mailman <mmailman@gmail.com>
2021-03-24 17:29:06 -0700

    Got the mouse over affect working and captured the correct color in the assert.

--- tests/location.js
+++ tests/location.js
@@ -3,8 +3,6 @@ describe('Location', function() {
   //Test to Verify that the Chicago location is present in the footer and that info is correctly displayed
   //title is "Chicago"
   //Phone number "+1 312 729 4500"
-  //Title inactive font color is "#808084"
-  //Title Hover color is "#fff"
   test('chicagoVerification', function(browser) {
     browser
       .url('https://www.designory.com')
@@ -15,13 +13,11 @@ describe('Location', function() {
       .assert.containsText('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', 'CHICAGO')
       //Looks like assert was returning an rgba code.  The rgba code for #808084 is rgba(128, 128, 132, 1)
       .assert.cssProperty('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', 'color', 'rgba(128, 128, 132, 1)', 'Testing if element <//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a> has an inactive font color of <#808084>')
-      .moveTo('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', function(){
+      .moveToElement('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', 5, 5, function(){
         browser.pause(10000)
-        browser.assert.cssProperty('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', 'color', '#fff')
+        // #fff is shorthand for #ffffff which is rgba(255, 255, 255, 1)
+        browser.assert.cssProperty('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a', 'color', 'rgba(255, 255, 255, 1)', 'Testing if element <//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a> has an active font color of <#fff>')
       })
       .end();
-      //.expect.element('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a').text.to.contain('CHICAGO')
-
-    //browser.expect.element('//*[@id="body"]/footer/div[1]/div/div/div[2]/div/h3/a').to.have.css('color').which.contains('#808084')
   });
 });
