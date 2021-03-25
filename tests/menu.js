df88f9cf8144241e07d70d0374ba785ceebc3c16
Michael Mailman <mmailman@gmail.com>
2021-03-25 12:21:57 -0700

    Fixing selectors and cleaning up comments

--- tests/menu.js
+++ tests/menu.js
@@ -2,13 +2,12 @@ function menuContentVerification (browser){
   //This function is used to verify that on all relevant pages menu options are the same
   browser
     .click('#nav-toggle')
-    //.click('.cookie-notice__close')
-    .assert.containsText('nav ul li:nth-child(1) a', 'WORK')
-    .assert.containsText('nav ul li:nth-child(2) a', 'ABOUT')
-    .assert.containsText('nav ul li:nth-child(3) a', 'CAREERS')
-    .assert.containsText('nav ul li:nth-child(4) a', 'LOCATIONS')
-    .assert.containsText('nav ul li:nth-child(5) a', 'CONTACT')  //For some reason Nightwatch is not finding the text
-    .assert.containsText('nav ul li:nth-child(6) a', 'NEWS')  //For some reason Nightwatch is not finding the text
+    .assert.containsText('nav > ul > li:nth-child(1) > a', 'WORK')
+    .assert.containsText('nav > ul > li:nth-child(2) > a', 'ABOUT')
+    .assert.containsText('nav > ul > li:nth-child(3) > a', 'CAREERS')
+    .assert.containsText('nav > ul > li:nth-child(4) > a', 'LOCATIONS')
+    .assert.containsText('nav > ul > li:nth-child(5) > a', 'CONTACT')
+    .assert.containsText('nav > ul > li:nth-child(6) > a', 'NEWS')
 }
 
 describe('Menu', function() {
@@ -25,10 +24,10 @@ describe('Menu', function() {
       .url('https://www.designory.com')
       .click('#nav-toggle')
       .pause(5000)
-      .click('nav ul li:nth-child(1) a')
+      .click('nav > ul > li:nth-child(1) > a')
       .assert.title('Designory Work & Case Studies')
       .assert.urlContains('/work')
-    //menuContentVerification(browser);
+    menuContentVerification(browser);
     browser.end();
   });
 
@@ -37,10 +36,10 @@ describe('Menu', function() {
       .url('https://www.designory.com')
       .click('#nav-toggle')
       .pause(5000)
-      .click('nav ul li:nth-child(2) a')
+      .click('nav > ul > li:nth-child(2) > a')
       .assert.title('Print & Digital Agency | Designory')
       .assert.urlContains('/about')
-    //menuContentVerification(browser);
+    menuContentVerification(browser);
     browser.end();
   });
 
@@ -49,56 +48,56 @@ describe('Menu', function() {
       .url('https://www.designory.com')
       .click('#nav-toggle')
       .pause(5000)
-      .click('nav ul li:nth-child(3) a')
+      .click('nav > ul > li:nth-child(3) > a')
       .assert.title('Careers | Designory')
       .assert.urlContains('/careers')
-    //menuContentVerification(browser);
+    menuContentVerification(browser);
     browser.end();
   });
 
+//Since this locationsNav test is just a verification of the locations in the menu, I didn't add the menuContentVerification function as it would be redundant as the first test covered that portion of the menu.
   test('locationsNav', function(browser) {
     browser
       .url('https://www.designory.com')
       .click('#nav-toggle')
       .pause(5000)
-      .click('nav ul li:nth-child(4) a')
-      .assert.containsText('.subnav li:nth-child(1) a', 'LONG BEACH')
-      .assert.containsText('.subnav li:nth-child(2) a', 'CHICAGO')
-      .assert.containsText('.subnav li:nth-child(3) a', 'NASHVILLE')
-      .assert.containsText('.subnav li:nth-child(4) a', 'TOKYO')
-      .assert.containsText('.subnav li:nth-child(5) a', 'LONDON')
-      .assert.containsText('.subnav li:nth-child(6) a', 'HONG KONG')
-      .assert.containsText('.subnav li:nth-child(7) a', 'PARIS')
-      .assert.containsText('.subnav li:nth-child(8) a', 'NEW YORK')
-      .assert.containsText('.subnav li:nth-child(9) a', 'NEW JERSEY')
-      .assert.containsText('.subnav li:nth-child(10) a', 'PORTLAND')
-      .assert.containsText('.subnav li:nth-child(11) a', 'PHILADELPHIA')
-    //menuContentVerification(browser);
-    browser.end();
+      .click('nav > ul  > li:nth-child(4) > a')
+      .assert.containsText('.subnav > li:nth-child(1) > a', 'LONG BEACH')
+      .assert.containsText('.subnav > li:nth-child(2) > a', 'CHICAGO')
+      .assert.containsText('.subnav > li:nth-child(3) > a', 'NASHVILLE')
+      .assert.containsText('.subnav > li:nth-child(4) > a', 'TOKYO')
+      .assert.containsText('.subnav > li:nth-child(5) > a', 'LONDON')
+      .assert.containsText('.subnav > li:nth-child(6) > a', 'HONG KONG')
+      .assert.containsText('.subnav > li:nth-child(7) > a', 'PARIS')
+      .assert.containsText('.subnav > li:nth-child(8) > a', 'NEW YORK')
+      .assert.containsText('.subnav > li:nth-child(9) > a', 'NEW JERSEY')
+      .assert.containsText('.subnav > li:nth-child(10) > a', 'PORTLAND')
+      .assert.containsText('.subnav > li:nth-child(11) > a', 'PHILADELPHIA')
+      .end();
   });
 
-  test('contactNav', function(browser) { //Running into same problem with the menuContentVerification
+  test('contactNav', function(browser) {
     browser
       .url('https://www.designory.com')
       .click('.cookie-notice__close')
       .click('#nav-toggle')
       .pause(5000)
-      .click('div.nav-wrapper.scroll nav ul li:nth-child(5) a')
+      .click('nav > ul > li:nth-child(5) > a')
       .assert.title('Contact | Designory')
       .assert.urlContains('/contact')
-    //menuContentVerification(browser);
+    menuContentVerification(browser);
     browser.end();
   });
 
-  test('newsNav', function(browser) { //Running into the same problem with the menuContentVerification
+  test('newsNav', function(browser) {
     browser
       .url('https://www.designory.com')
       .click('#nav-toggle')
       .pause(5000)
-      .click('div.nav-wrapper.scroll nav ul li:nth-child(6) a')
+      .click('nav > ul > li:nth-child(6) > a')
       .assert.title('Designory News & Awards | Designory')
       .assert.urlContains('/news')
-    //menuContentVerification(browser);
+    menuContentVerification(browser);
     browser.end();
   });
 });
